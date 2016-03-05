09fb946b54a77848b6e8a7f907dda715a44c6b4a
Rob <Home@iMac.local>
2016-03-05 09:28:48 -0500

    Add Sample Project to GitHub account
    Added First iPhone App to account

---
+++ First iPhone App Project/First iPhone App Project/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  First iPhone App Project
+//
+//  Created by Rob on 2015-11-1.
+//  Copyright © 2015 Lounging Rabbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+
+    print("Hi my name's Rob")
+  }
+
+
+}
+
